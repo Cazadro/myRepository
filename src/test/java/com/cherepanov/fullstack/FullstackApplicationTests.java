2e22e1be9266d1f89222428a34a89e9eeadc2ac4
illia.cherepanov <c.illia@student.khai.edu>
2024-05-20 12:48:30 +0300

    failure!

--- src/test/java/com/cherepanov/fullstack/FullstackApplicationTests.java
+++ src/test/java/com/cherepanov/fullstack/FullstackApplicationTests.java
@@ -2,17 +2,14 @@ package com.cherepanov.fullstack;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.event.EventListener;
-import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
 class FullstackApplicationTests {
 
 	@Test
 	void contextLoads() {
-//		Assertions.fail("Test fail!");
+		Assertions.fail("Test fail!");
 	}
+
 }
