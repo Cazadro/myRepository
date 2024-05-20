3d3ab3d5a1b8b8a00a400b96a53951dd02a90a9c
illia.cherepanov <c.illia@student.khai.edu>
2024-05-20 14:59:04 +0300

    save fail in test

--- src/test/java/com/cherepanov/fullstack/FullstackApplicationTests.java
+++ src/test/java/com/cherepanov/fullstack/FullstackApplicationTests.java
@@ -2,7 +2,11 @@ package com.cherepanov.fullstack;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.event.EventListener;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
 class FullstackApplicationTests {
@@ -11,5 +15,4 @@ class FullstackApplicationTests {
 	void contextLoads() {
 		Assertions.fail("Test fail!");
 	}
-
 }
