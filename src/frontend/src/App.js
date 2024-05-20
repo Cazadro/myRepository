24706c5538ac7dfad97ecaa0bcdaf76b727cb78c
illia.cherepanov <c.illia@student.khai.edu>
2024-05-20 17:42:48 +0300

    changed deploy and add footer

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -23,7 +23,7 @@ import {
     Spin,
     Table,
     theme,
-    message, Image
+    message, Image, Divider
 } from 'antd';
 import StudentDrawerForm from "./StudentDrawerForm";
 import {errorNotification, successNotification} from "./Notifications";
@@ -227,6 +227,7 @@ function App() {
                     width={75}
                     src="https://private-user-images.githubusercontent.com/91321103/331877796-091f75d5-157c-4c9c-9a6d-f7cc1763e416.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTYxMjgzNjUsIm5iZiI6MTcxNjEyODA2NSwicGF0aCI6Ii85MTMyMTEwMy8zMzE4Nzc3OTYtMDkxZjc1ZDUtMTU3Yy00YzljLTlhNmQtZjdjYzE3NjNlNDE2LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA1MTklMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNTE5VDE0MTQyNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWJlNzM1OGYyOWM3NjM1M2FhZTUwMDAwN2UwMmM2MmY3YjNhYjMzYjMxODg5MTBmNzVjNGZmM2I0OTVmYjg1MzYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JmFjdG9yX2lkPTAma2V5X2lkPTAmcmVwb19pZD0wIn0.JlKcmEsgU7HmCXHom9iADzHSb2zD6CZF3_IN9UU-4yc"
                 />
+                <Divider>Text</Divider>
             </Footer>
         </Layout>
     </Layout>
